52e34f556304d3973e38d3b2d919d9add7f5f056
honghu91 <honghu91@hotmail.com>
2014-03-06 20:20:17 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -15,11 +15,11 @@
                 list: [
                     {intro: "Chrome插件支持", subIntro: "支持Chrome插件方式，轻松打开视觉稿进行体验、开发、测试<p class='point'><a href='https://chrome.google.com/webstore/detail/alloydesigner/ojooeaohlmgpcjajikhmibcnbebfenid?hl=zh-CN&authuser=1' target='_blank' class='getPlugin'>立即安装插件</a></p>", color: "rgb(41, 50, 225)"},
                     {intro: "嵌入代码库方式，全面支持IE7+", subIntro: "&lt;script src='alloydesigner.js' type='text/javascript'>&lt;/script><p class='point'><a href='release/alloydesigner.js' download='alloydesigner.js' target='_blank'  class='getPlugin'>立即下载使用</a></p>", color: "rgb(203, 0, 9)"},
-                    {intro: "收藏夹工具支持，无需手动嵌入代码", subIntro: "添加至Chrome、Mordern IE收藏栏使用", color: "rgb(30, 170, 56)"},
-                    {intro: "强大记忆恢复功能", subIntro: "刷新后恢复上次状态，无需重新调整视觉稿位置", color: "rgb(255, 77, 16)"},
-                    {intro: "便利的快捷键操作", subIntro: "使用易记的快捷键操作，进一步提高开发效率", color: "rgb(255, 77, 16)"},
-                    {intro: "稳固的基础功能", subIntro: "固定视觉稿，调节透明度，测试开发等基础功能强健", color: "rgb(255, 77, 16)"},
-                    {intro: "丰富的开发辅助工具", subIntro: "包含测距工具、CSS助手与取色器等快捷工具", color: "rgb(255, 77, 16)"},
+                    {intro: "收藏夹工具支持", subIntro: "无需手动嵌入代码,添加至Chrome、Mordern IE收藏栏使用 <p class='point'><a href='javascript:(function(){var w=window,d=w.document,b=d.body,s=d.createElement(\"script\");s.src=\"http://alloyteam.github.io/AlloyDesigner/release/alloydesigner.js\";b.appendChild(s);})();' target='_blank' class='getPlugin'>拖拽至收藏</a></p>", color: "rgb(30, 170, 56)"},
+                    {intro: "强大记忆恢复功能", subIntro: "刷新后恢复上次状态，无需重新调整视觉稿位置", color: "rgb(255, 77, 16)", className: "feture"},
+                    {intro: "便利的快捷键操作", subIntro: "使用易记的快捷键操作，进一步提高开发效率", color: "rgb(255, 77, 16)", className: "feture"},
+                    {intro: "稳固的基础功能", subIntro: "固定视觉稿，调节透明度，测试开发等基础功能强健", color: "rgb(255, 77, 16)", className: "feture"},
+                    {intro: "丰富的开发辅助工具", subIntro: "包含测距工具、CSS助手与取色器等快捷工具", color: "rgb(255, 77, 16)", className: "feture"}
                 ]
             };
 
